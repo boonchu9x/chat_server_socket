00268ce155f8c147c66c6505d0f094e4d21b7068
boonchu9x <developer.nguyentruong@gmai.com>
2020-06-08 02:54:40 +0700

    change

--- app.js
+++ app.js
@@ -11,69 +11,77 @@ app.get("/", function(req, res){
 });
 
 var arrayUserName = [];
-
 //server 
 io.sockets.on('connection', function (socket) {
 	
 	console.log("client conneted");
- 
 	//add user register to arrayUserName
-	socket.on('client-register', function(data){
-	console.log("client just registed username =" + data);
-	var result = false;
-	//check in arrayUserName
-	if(arrayUserName.indexOf(data) > -1){
-		console.log("This account already exists!")
-		result = false;
-	}
-	else{
-		arrayUserName.push(data);
-		socket.un = data;
-		console.log("Sign in success");
-		result = true;
-	}
-	socket.emit('login', {register : result});	
+	socket.on('client-register', function(username){
+		console.log("client just registed username = " + username);
+		var result = false;
+		//check in arrayUserName
+		if(arrayUserName.indexOf(username) > -1){
+			console.log("This account already exists!")
+			result = false;
+		}
+		else{
+			arrayUserName.push(username);
+			socket.username = username;
+			console.log("Sign in success");
+			result = true;
+		}
+		socket.emit('signin', {register : result});	
 	
 	});
 	
-	socket.on('client-joined-chat', function(data){
-		console.log("user joined username =" + data);
+	socket.on('login', function(username){
+		var result_login = false;
+		//check in arrayUserName
+		if(arrayUserName.indexOf(username) == -1){
+			arrayUserName.push(username);
+			socket.username = username;
+			console.log("push " + username + " to array");
+		}
+	});
+	
+	socket.on('client-joined-chat', function(username){
+		console.log("user joined username = " + username);
 		//echo globally (all clients) that a person has connected
-		io.sockets.emit('server-send-user-joined', {username: data, number: arrayUserName.length});
+		io.sockets.emit('client-joined-chat', {username: username, number: arrayUserName.length});
 	});
 	
-	io.sockets.emit('server-send-user-joined', {username: socket.username});
-
 	socket.on('client-send-chat', function (chat) {
-	  console.log(socket.un + ":" + chat);
+	  console.log(socket.username + ":" + chat);
 		// emit toi tat ca moi nguoi
-		io.sockets.emit('server-send-chat', {username: socket.un, message: chat});
+		socket.broadcast.emit('client-send-chat', {username: socket.username, message: chat});
 	
 		// emit tới máy nguoi vừa gửi
-		//socket.emit('server-send-chat', { comment: socket.un + ":" + chat });
+		//socket.emit('server-send-chat', {username: socket.client-stop-typing, message: chat});
 	});
   
 	
 	socket.on('client-send-typing', function () {
-		console.log(socket.un + " is typing");
-		io.sockets.emit('server-send-typing', {username: socket.un});
+		console.log(socket.username + " is typing");
+		socket.broadcast.emit('client-send-typing', {username: socket.username});
 	});
   
 	
 	socket.on('client-stop-typing', () => {
-		console.log(socket.un + " stop typing");
-		io.sockets.emit('server-send-typing', {username: socket.un});
+		console.log(socket.username + " stop typing");
+		socket.broadcast.emit('server-send-stop-typing', {username: socket.username});
 	});
   
   
 	socket.on('disconnect', function(){
+		console.log(socket.username + " disconnect");
 		if(arrayUserName.length > 0){
-			var pos = arrayUserName.indexOf(socket.un);
+			var pos = arrayUserName.indexOf(socket.username);
 			if(pos > -1){
 				arrayUserName.splice(pos, 1);
-				io.sockets.emit('server-send-user-left', {usernameleft: socket.un, number: arrayUserName.length});
+				io.sockets.emit('user-left', {usernameleft: socket.username, number: arrayUserName.length});
 			}
 		}
+		
     });
  
 });
